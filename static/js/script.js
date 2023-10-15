dc2410e8496a23113ed8ca7d8ea10d21a8a4f532
chris macharia <christophermaina003@gmail.com>
2023-10-15 10:13:42 -0400

    Correct the navbar and footer to display correctly

---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+// script responsible for footer behaviour
+const footer = document.getElementById('page-footer');
+
+document.addEventListener('mousemove', (event) => {
+    const mouseY = event.clientY;
+
+    // Adjust this value to control when the footer appears
+    const threshold = 10;
+
+    if (window.innerHeight - mouseY < threshold) {
+        footer.classList.remove('hidden');
+    } else {
+        footer.classList.add('hidden');
+    }
+});
